9b2f69642bf4cb43bdb1cace650e6e95be134a32
hay-oon <hayooncode@gmail.com>
2025-08-12 20:05:26 +0900

    Refactor: Message Dtos

--- src/main/java/com/example/demo/controller/dto/MessageCreateRequestDto.java
+++ src/main/java/com/example/demo/controller/dto/MessageCreateRequestDto.java
@@ -1,5 +1,10 @@
 package com.example.demo.controller.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class MessageCreateRequestDto {
     private String content;
     private Integer userId;
--- src/main/java/com/example/demo/controller/dto/MessageResponseDto.java
+++ src/main/java/com/example/demo/controller/dto/MessageResponseDto.java
@@ -1,7 +1,14 @@
 package com.example.demo.controller.dto;
 
+import com.example.demo.repository.entity.Message;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.time.LocalDateTime;
 
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class MessageResponseDto {
     private Integer id;
     private String content;
@@ -9,6 +16,15 @@ public class MessageResponseDto {
     private Integer userId;
     private String username;
 
+    public static MessageResponseDto from (Message entity) {
+        return new MessageResponseDto(
+                entity.getId(),
+                entity.getContent(),
+                entity.getCreatedAt(),
+                entity.getUser().getId(),
+                entity.getUser().getUsername()
+        );
+    }
 }
 
 
